9ba3a185374be8a06b333fcc1966edeee288616f
hhomma <hhomma01gmail.com>
2021-05-15 23:07:51 +0900

    init project

---
+++ src/main/kotlin/com/test/option/request/requestoptiontest/RequestOptionTestApplication.kt
@@ -0,0 +1,11 @@
+package com.test.option.request.requestoptiontest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestOptionTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestOptionTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/option/request/requestoptiontest/RequestOptionTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.option.request.requestoptiontest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RequestOptionTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
